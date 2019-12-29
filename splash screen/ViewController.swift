60664559a4fb5c1ca8c1ff5e0d5a54283614a952
duxetech <karthikriches@gmail.com>
2019-12-29 15:15:25 +0530

    Initial Commit

---
+++ splash screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  splash screen
+//
+//  Created by Karthik on 29/12/19.
+//  Copyright © 2019 Karthik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
